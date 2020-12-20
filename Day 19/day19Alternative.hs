c4bca13188a55108445dafee08db96db647214f9
Thomas Stacey <Thomas.Stacey@warwick.ac.uk>
2020-12-20 22:14:58 +0000

    Day 20

--- Day 19/day19Alternative.hs
+++ Day 19/day19Alternative.hs
@@ -1,17 +1,16 @@
 module Day19Alternative where
   import Data.List.Split
-  import Data.Map.Strict (Map)
-  import qualified Data.Map.Strict as Map
-  import Data.Either
+  import Data.IntMap.Strict (IntMap)
+  import qualified Data.IntMap.Strict as IntMap
   import Data.List (nub)
-  import Data.Maybe
+  import Data.Maybe (fromJust)
 
   main :: IO ()
   main = do
     [rs, msgs] <- map lines . splitOn "\n\n" <$> readFile "input.txt"
-    let rules = Map.fromList $ map processRule rs
+    let rules = IntMap.fromList $ map processRule rs
     print $ length $ filter (doesParse rules) msgs
-    print $ length $ filter (doesParse (Map.union (Map.fromList $ map processRule ["8: 42 | 42 8", "11: 42 31 | 42 11 31"]) rules)) msgs
+    print $ length $ filter (doesParse (IntMap.union (IntMap.fromList $ map processRule ["8: 42 | 42 8", "11: 42 31 | 42 11 31"]) rules)) msgs
 
   processRule :: String -> (Int, Either Char [[Int]])
   processRule s = (read l, getRHS r)
@@ -20,15 +19,15 @@ module Day19Alternative where
       getRHS :: String -> Either Char [[Int]]
       getRHS rhs@(r:rs)
         | r == '\"' = Left  $ head rs
-        | otherwise = Right $ map (map read . words) $ splitOn " | " rhs
+        | otherwise = Right $ map read . words <$> splitOn " | " rhs
 
-  doesParse :: Map Int (Either Char [[Int]]) -> String -> Bool
-  doesParse rules word = "" `elem` parseWithRule rules 0 [word]
+  doesParse :: IntMap (Either Char [[Int]]) -> String -> Bool
+  doesParse rules word = "" `elem` parseWithRule rules [word] 0
 
-  parseWithRule :: Map Int (Either Char [[Int]]) -> Int -> [String] -> [String]
-  parseWithRule rMap rule ss = nub $ concatMap (doParse rMap rule) $ filter (not . null) ss
+  parseWithRule :: IntMap (Either Char [[Int]]) -> [String] -> Int -> [String]
+  parseWithRule rMap ss rule = nub $ filter (not . null) ss >>= doParse rMap rule
     where
-      doParse :: Map Int (Either Char [[Int]]) -> Int -> String -> [String]
-      doParse rMap rule str@(s:ss) = case fromJust $ Map.lookup rule rMap of
+      doParse :: IntMap (Either Char [[Int]]) -> Int -> String -> [String]
+      doParse rMap rule str@(s:ss) = case fromJust $ IntMap.lookup rule rMap of
         Left c -> if s == c then [ss] else []
-        Right rss -> concatMap (foldl (flip $ parseWithRule rMap) [str]) rss
+        Right rss -> rss >>= foldl (parseWithRule rMap) [str]
